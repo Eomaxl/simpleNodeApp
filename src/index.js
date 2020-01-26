2465d5723f4b8fe4b5b120fd4324f1fd00680ebd
Sourav Mansingh <souravmansingh@Souravs-MacBook-Air.local>
2020-01-26 23:42:34 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.get('/',(req,res)=>{
@@ -9,4 +9,4 @@ app.get('/',(req,res)=>{
 
 app.listen(PORT,()=>{
     console.log("Server is running")
-})
+});
